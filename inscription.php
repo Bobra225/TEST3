bc3dda471d8f1324c806c98ff89d9849f7b924fb
Bobra225 <82872368+Bobra225@users.noreply.github.com>
2023-12-09 18:37:29 +0000

    Add files via upload

--- inscription.php
+++ inscription.php
@@ -1,4 +1,9 @@
 <?php
+use PHPMailer\PHPMailer\PHPMailer;
+use PHPMailer\PHPMailer\Exception;
+
+require 'vendor/autoload.php';
+
 if ($_SERVER["REQUEST_METHOD"] == "POST") {
     // Récupérer les données du formulaire
     $nom = htmlspecialchars($_POST["nom"]);
@@ -23,15 +28,29 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
     $sql = "INSERT INTO utilisateurs (nom, email, mot_de_passe) VALUES ('$nom', '$email', '$mot_de_passe')";
 
     if ($conn->query($sql) === TRUE) {
-        // Envoi d'un e-mail
-        $destinataire = "czabo@outlook.fr";
-        $sujet = "Nouvelle inscription";
-        $message = "Nom: $nom\nEmail: $email\nMot de passe: *** (haché pour des raisons de sécurité)";
+        // Envoi d'un e-mail avec PHPMailer
+        $mail = new PHPMailer(true);
 
-        // Utilisez la fonction mail() pour envoyer l'e-mail
-        mail($destinataire, $sujet, $message);
+        try {
+            $mail->isSMTP();
+            $mail->Host = 'smtp.gmail.com';
+            $mail->SMTPAuth = true;
+            $mail->Username = 'votre_adresse@gmail.com';
+            $mail->Password = 'votre_mot_de_passe_gmail';
+            $mail->SMTPSecure = 'tls';
+            $mail->Port = 587;
 
-        echo "Inscription réussie! Un e-mail a été envoyé à $destinataire avec les informations.";
+            $mail->setFrom('votre_adresse@gmail.com', 'Votre Nom');
+            $mail->addAddress('czabo.fr'); // Adresse à laquelle vous souhaitez envoyer l'e-mail
+            $mail->Subject = 'Nouvelle inscription';
+            $mail->Body = "Nom: $nom\nEmail: $email\nMot de passe: *** (haché pour des raisons de sécurité)";
+
+            $mail->send();
+
+            echo "Inscription réussie! Un e-mail a été envoyé avec les informations.";
+        } catch (Exception $e) {
+            echo "Erreur lors de l'envoi de l'e-mail : {$mail->ErrorInfo}";
+        }
     } else {
         echo "Erreur: " . $sql . "<br>" . $conn->error;
     }
