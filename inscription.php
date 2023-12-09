b20eacfe7e56fb7d094fde0d05539d83cb0808bc
Bobra225 <82872368+Bobra225@users.noreply.github.com>
2023-12-09 20:59:05 +0000

    Add files via upload

--- inscription.php
+++ inscription.php
@@ -1,21 +1,57 @@
 <?php
-if ($_SERVER["REQUEST_METHOD"] == "POST") {
-    // Récupérer les données du formulaire
-    $nom = htmlspecialchars($_POST["nom"]);
-    $email = htmlspecialchars($_POST["email"]);
-    $mot_de_passe = password_hash($_POST["mot_de_passe"], PASSWORD_DEFAULT);
+require 'vendor/autoload.php';
 
-    // Vous pouvez maintenant traiter les données, par exemple, les enregistrer dans un fichier ou une base de données
+use SendGrid\Mail\From;
+use SendGrid\Mail\To;
+use SendGrid\Mail\Mail;
+use SendGrid\Mail\Subject;
+use SendGrid\Mail\PlainTextContent;
+
+if ($_SERVER["REQUEST_METHOD"] == "GET") {
+    // Récupérer les données du formulaire
+    $nom = isset($_GET["nom"]) ? htmlspecialchars($_GET["nom"]) : "";
+    $email = isset($_GET["email"]) ? htmlspecialchars($_GET["email"]) : "";
+    $mot_de_passe = isset($_GET["mot_de_passe"]) ? password_hash($_GET["mot_de_passe"], PASSWORD_DEFAULT) : "";
+
+    // Vous pouvez maintenant traiter les données, par exemple, les enregistrer dans une base de données
     // Dans un environnement de production, assurez-vous de mettre en œuvre des mesures de sécurité appropriées.
 
-    // Enregistrement dans un fichier texte (ajustez le chemin selon votre besoin)
-    $file = 'enregistrements.txt';
+    // Exemple de connexion à une base de données MySQL (à adapter selon votre configuration)
+    $servername = "localhost";
+    $username = "votre_nom_utilisateur";
+    $password = "votre_mot_de_passe";
+    $dbname = "votre_base_de_donnees";
 
-    $data = "Nom: $nom\nEmail: $email\nMot de passe: $mot_de_passe\n\n";
+    $conn = new mysqli($servername, $username, $password, $dbname);
 
-    // Écriture dans le fichier
-    file_put_contents($file, $data, FILE_APPEND | LOCK_EX);
+    if ($conn->connect_error) {
+        die("La connexion à la base de données a échoué : " . $conn->connect_error);
+    }
 
-    echo "Inscription réussie! Les informations ont été enregistrées.";
+    $sql = "INSERT INTO utilisateurs (nom, email, mot_de_passe) VALUES ('$nom', '$email', '$mot_de_passe')";
+
+    if ($conn->query($sql) === TRUE) {
+        // Envoi d'un e-mail avec SendGrid
+        $sendgridApiKey = 'VOTRE_CLE_API_SENDGRID'; // Remplacez par votre clé API SendGrid
+
+        $email = new Mail();
+        $email->setFrom(new From("christianzabo225@gmail.com", "Votre Nom"));
+        $email->addTo(new To("czabo@outlook.fr", "Destinataire"));
+        $email->setSubject(new Subject("Nouvelle inscription"));
+        $email->addContent(new PlainTextContent("Nom: $nom\nEmail: $email\nMot de passe: *** (haché pour des raisons de sécurité)"));
+
+        $sendgrid = new \SendGrid($sendgridApiKey);
+
+        try {
+            $response = $sendgrid->send($email);
+            echo "Inscription réussie! Les informations ont été envoyées par e-mail.";
+        } catch (Exception $e) {
+            echo "Erreur lors de l'envoi de l'e-mail : {$e->getMessage()}";
+        }
+    } else {
+        echo "Erreur: " . $sql . "<br>" . $conn->error;
+    }
+
+    $conn->close();
 }
 ?>
